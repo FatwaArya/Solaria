baf374fb763dac53297b78c6c8e52f08cef7607b
FatwaArya <fatwaaryasatyaakbar@gmail.com>
2021-09-12 21:22:13 +0700

    icon title and other

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 window.addEventListener("scroll", function(){
     let navbar = document.querySelector('.navbar');
-    navbar.classList.toggle('sticky', window.scrollY >937);
+    navbar.classList.toggle('sticky', window.scrollY >800);
 })
 // sticky tutorialhttps://www.youtube.com/watch?v=6HFpw5fcaD8
---
+++ galaxies.js
@@ -0,0 +1,6 @@
+
+let andromeda = document.getElementById('andromeda')
+
+andromeda.addEventListener('click',function(){
+    alert('This information is currently unavailable ');
+})
