a8e7144de88ea1e7a726f5e21ea4984277cfc044
FatwaArya <fatwaaryasatyaakbar@gmail.com>
2021-09-15 21:33:40 +0700

    10

--- app.js
+++ app.js
@@ -3,3 +3,20 @@ window.addEventListener("scroll", function(){
     navbar.classList.toggle('sticky', window.scrollY >800);
 })
 // sticky tutorialhttps://www.youtube.com/watch?v=6HFpw5fcaD8
+
+
+//Get the button:
+let mybutton = document.getElementById("top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 400) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+//bottom to top https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
